4d8cd96fd6cb39ff25990de71868d2cdfd12e998
Jvictorio09 <juliavictorio16@gmail.com>
2026-02-10 01:30:55 +0800

    udpated

--- myProject/settings.py
+++ myProject/settings.py
@@ -19,13 +19,14 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-7-9br=zyg7m$!ewa^nlsg+&#e+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'kane-sproject-production.up.railway.app', 'danielwoodcourses-production.up.railway.app']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'kane-sproject-production.up.railway.app', 'danielwoodcourses-production.up.railway.app', 'edmarincourse-production.up.railway.app']
 
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost',
     'https://localhost',
     'https://kane-sproject-production.up.railway.app',
     'https://danielwoodcourses-production.up.railway.app',
+    'https://edmarincourse-production.up.railway.app',
 ]
 
 # Application definition
